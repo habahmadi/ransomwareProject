9baba1a0be45f573bd0d77472243a4d43c693a1c
Habibullah Ahmadi <habahmadi05@outlook.com>
2026-03-11 12:49:03 +0000

    Added folder path setup for monitoring

--- src/file_monitor.py
+++ src/file_monitor.py
@@ -6,3 +6,12 @@ import time     # used to keep monitor running
 # watchdog used to monitor file system events
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+# now we need to getthe path to the test_environment folder
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# go one level up from root
+project_root = os.path.dirname(current_dir)
+
+# path to the folder we want to monitor
+WATCH_FOLDER = os.path.join(project_root, "test_environment")
