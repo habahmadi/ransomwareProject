13252599e364a2629b082908f29524e14e13d706
Habibullah Ahmadi <habahmadi05@outlook.com>
2026-03-11 12:55:54 +0000

    Created event handler class for file changes

--- src/file_monitor.py
+++ src/file_monitor.py
@@ -15,3 +15,23 @@ project_root = os.path.dirname(current_dir)
 
 # path to the folder we want to monitor
 WATCH_FOLDER = os.path.join(project_root, "test_environment")
+
+
+# this class describes what to do when these file events happen (create, modify, delete, move)
+class MyHandler(FileSystemEventHandler):
+
+    # runs when a new file is created
+    def on_created(self, event):
+        print("File created:", event.src_path)
+
+    # runs when a file is modified
+    def on_modified(self, event):
+        print("File modified:", event.src_path)
+
+    # runs when a file is deleted
+    def on_deleted(self, event):
+        print("File deleted:", event.src_path)
+
+    # runs when a file is moved or renamed
+    def on_moved(self, event):
+        print("File moved or renamed:", event.src_path)
