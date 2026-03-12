3c18d56a5b459d8a6ab213558fdcf82f4c53f1cd
Habibullah Ahmadi <habahmadi05@outlook.com>
2026-03-12 12:34:17 +0000

    Testing first file creation to simulator

--- src/simulator.py
+++ src/simulator.py
@@ -13,3 +13,19 @@ project_root = os.path.dirname(current_dir)
 
 # path to the folder where test files will be created
 TEST_FOLDER = os.path.join(project_root, "test_environment")
+
+# this function creates a normal test file
+def create_file():
+
+    file_name = "normal_file_1.txt"
+    file_path = os.path.join(TEST_FOLDER, file_name)
+
+    with open(file_path, "w") as file:
+        file.write("This is a normal test file.\n")
+
+    print("Created file:", file_path)
+
+# this simply runs the simulator
+if __name__ == "__main__":
+
+    create_file()
